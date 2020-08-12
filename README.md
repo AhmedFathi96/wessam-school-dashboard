962893860769ca6a210eefefa30cac02bc2cb483
Ahmed Fathi <afathy@koinz.app>
2020-08-12 02:56:29 +0200

    dashboard content expect courses section

--- README.md
+++
@@ -1,2 +0,0 @@
-# wessam-school-dashboard
-React.js , Redux , axios , saga , HOOKS based dashboard used to control website content
--- src/Axios/urls.ts
+++ src/Axios/urls.ts
@@ -1,53 +1,53 @@
-export const loginUTR     = 'http://161.35.115.193:5026/app/api/login';
+export const loginUTR     = 'http://161.35.115.193:5026/api/login';
 
 
-export const getAdminsURL = "http://161.35.115.193:5026/app/api/admin/get-admins";
-export const createAdminsURL = "http://161.35.115.193:5026/app/api/admin/create-admin";
-export const editAdminsURL = (id:string) => `http://161.35.115.193:5026/app/api/admin/update-admin/${id}`;
-export const deleteAdminsURL = (id:string) => `http://161.35.115.193:5026/app/api/admin/delete-admin/${id}`;
+export const getAdminsURL = "http://161.35.115.193:5026/api/admin/get-admins";
+export const createAdminsURL = "http://161.35.115.193:5026/api/admin/create-admin";
+export const editAdminsURL = (id:string) => `http://161.35.115.193:5026/api/admin/update-admin/${id}`;
+export const deleteAdminsURL = (id:string) => `http://161.35.115.193:5026/api/admin/delete-admin/${id}`;
 
 
-export const getStudentsURL = "http://161.35.115.193:5026/app/api/Student/get-Students";
-export const createStudentURL = "http://161.35.115.193:5026/app/api/Student/create-Student";
-export const editStudentURL = (id:string) => `http://161.35.115.193:5026/app/api/Student/update-Student/${id}`;
-export const deleteStudentURL = (id:string) => `http://161.35.115.193:5026/app/api/Student/delete-Student/${id}`;
-
-
-
-export const getSliderItemsURL = "http://161.35.115.193:5026/app/api/slider/get-sliders";
-export const createSliderItemsURL = "http://161.35.115.193:5026/app/api/slider/add-slider";
-export const editSliderItemsURL = (id:string) => `http://161.35.115.193:5026/app/api/slider/update-slider/${id}`;
-export const deleteSliderItemsURL = (id:string) => `http://161.35.115.193:5026/app/api/slider/delete-slider/${id}`;
-
-
-export const getAboutURL = "http://161.35.115.193:5026/app/api/about/get-about";
-export const createAboutURL = "http://161.35.115.193:5026/app/api/about/create-about";
-export const editAboutURL = (id:string) => `http://161.35.115.193:5026/app/api/about/update-about/${id}`;
-export const deleteAboutURL = (id:string) => `http://161.35.115.193:5026/app/api/about/delete-about/${id}`;
-
-export const getTestimonialsURL = "http://161.35.115.193:5026/app/api/testimonial/get-testimonials";
-export const createTestimonialURL = "http://161.35.115.193:5026/app/api/testimonial/add-testimonial";
-export const editTestimonialURL = (id:string) => `http://161.35.115.193:5026/app/api/testimonial/update-testimonial/${id}`;
-export const deleteTestimonialURL = (id:string) => `http://161.35.115.193:5026/app/api/testimonial/delete-testimonial/${id}`;
+export const getStudentsURL = "http://161.35.115.193:5026/api/Student/get-Students";
+export const createStudentURL = "http://161.35.115.193:5026/api/Student/create-Student";
+export const editStudentURL = (id:string) => `http://161.35.115.193:5026/api/Student/update-Student/${id}`;
+export const deleteStudentURL = (id:string) => `http://161.35.115.193:5026/api/Student/delete-Student/${id}`;
 
 
 
-export const getGalleryImagesURL = "http://161.35.115.193:5026/app/api/gallery/get-gallery-images";
-export const createGalleryImageURL = "http://161.35.115.193:5026/app/api/gallery/add-gallery-image";
-export const editGalleryImageURL = (id:string) => `http://161.35.115.193:5026/app/api/gallery/update-gallery-image/${id}`;
-export const deleteGalleryImageURL = (id:string) => `http://161.35.115.193:5026/app/api/gallery/delete-gallery-image/${id}`;
+export const getSliderItemsURL = "http://161.35.115.193:5026/api/slider/get-sliders";
+export const createSliderItemsURL = "http://161.35.115.193:5026/api/slider/add-slider";
+export const editSliderItemsURL = (id:string) => `http://161.35.115.193:5026/api/slider/update-slider/${id}`;
+export const deleteSliderItemsURL = (id:string) => `http://161.35.115.193:5026/api/slider/delete-slider/${id}`;
 
 
-export const getBlogPostsURL = "http://161.35.115.193:5026/app/api/blog/get-blog-posts";
-export const createBlogPostURL = "http://161.35.115.193:5026/app/api/blog/add-blog-post";
-export const editBlogPostURL = (id:string) => `http://161.35.115.193:5026/app/api/blog/update-blog-post/${id}`;
-export const deleteBlogPostURL = (id:string) => `http://161.35.115.193:5026/app/api/blog/delete-blog-post/${id}`;
+export const getAboutURL = "http://161.35.115.193:5026/api/about/get-about";
+export const createAboutURL = "http://161.35.115.193:5026/api/about/create-about";
+export const editAboutURL = (id:string) => `http://161.35.115.193:5026/api/about/update-about/${id}`;
+export const deleteAboutURL = (id:string) => `http://161.35.115.193:5026/api/about/delete-about/${id}`;
+
+export const getTestimonialsURL = "http://161.35.115.193:5026/api/testimonial/get-testimonials";
+export const createTestimonialURL = "http://161.35.115.193:5026/api/testimonial/add-testimonial";
+export const editTestimonialURL = (id:string) => `http://161.35.115.193:5026/api/testimonial/update-testimonial/${id}`;
+export const deleteTestimonialURL = (id:string) => `http://161.35.115.193:5026/api/testimonial/delete-testimonial/${id}`;
+
+
+
+export const getGalleryImagesURL = "http://161.35.115.193:5026/api/gallery/get-gallery-images";
+export const createGalleryImageURL = "http://161.35.115.193:5026/api/gallery/add-gallery-image";
+export const editGalleryImageURL = (id:string) => `http://161.35.115.193:5026/api/gallery/update-gallery-image/${id}`;
+export const deleteGalleryImageURL = (id:string) => `http://161.35.115.193:5026/api/gallery/delete-gallery-image/${id}`;
+
+
+export const getBlogPostsURL = "http://161.35.115.193:5026/api/blog/get-blog-posts";
+export const createBlogPostURL = "http://161.35.115.193:5026/api/blog/add-blog-post";
+export const editBlogPostURL = (id:string) => `http://161.35.115.193:5026/api/blog/update-blog-post/${id}`;
+export const deleteBlogPostURL = (id:string) => `http://161.35.115.193:5026/api/blog/delete-blog-post/${id}`;
 
 
 
 
-export const getContactsURL = "http://161.35.115.193:5026/app/api/contact/get-contacts";
-export const creatContactURL = "http://161.35.115.193:5026/app/api/contact/create-contact";
-export const deleteContactURL = (id:string) => `http://161.35.115.193:5026/app/api/contact/delete-contact/${id}`;
+export const getContactsURL = "http://161.35.115.193:5026/api/contact/get-contacts";
+export const creatContactURL = "http://161.35.115.193:5026/api/contact/create-contact";
+export const deleteContactURL = (id:string) => `http://161.35.115.193:5026/api/contact/delete-contact/${id}`;
 
 
